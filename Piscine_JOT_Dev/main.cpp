8d5a18533ef1b6def8237ac83bd944a308273735
TTL30 <tiago.teixeira-lopes@edu.ece.fr>
2019-04-15 12:15:44 +0200

    ajout fichier codblocks

--- Piscine_JOT_Dev/main.cpp
+++ Piscine_JOT_Dev/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "svgfile.h"
+#include "graphe.h"
+#include "sommet.h"
 
-using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Svgfile svgout;
+    graphe g{"files/broadway.txt"};
+    g.afficher(svgout);
     return 0;
 }
